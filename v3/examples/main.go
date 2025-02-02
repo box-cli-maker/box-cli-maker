7c0601f1ab31998bf218163fda0b81c656ffc5b6
Swastik <swstkbaranwal@gmail.com>
2025-02-03 02:26:28 +0530

    add current progress, more to follwo up

--- v3/examples/main.go
+++ v3/examples/main.go
@@ -7,7 +7,20 @@ import (
 )
 
 func main() {
-	bx := box.NewBox().Width(0).Height(0).Color("#26552e")
-	b, _ := bx.Render("Box CLI Maker", "Make Highly Customizable Boxes for CLI")
+	bx := box.NewBox().Width(0).Height(0).Color("#26552e").TitlePosition(box.Top).Style(box.Single).TitleColor("#427ef5").ContentColor("#f5b342")
+	b, _ := bx.Render("Box CLI Maker", "Make Highly Customizable \tBoxes for CLI")
+	fmt.Println(b)
+
+	by := box.NewBox().
+		Width(2).
+		Height(3).TitlePosition(box.Inside).
+		SetTopRight("xyxy").
+		SetTopLeft("xyxy").
+		SetBottomRight("xyxy").
+		SetBottomLeft("xyxy").
+		SetHorizontal("-").
+		SetVertical("|")
+
+	b, _ = by.Render("Box CLI Maker", "Another custom box type")
 	fmt.Println(b)
 }
